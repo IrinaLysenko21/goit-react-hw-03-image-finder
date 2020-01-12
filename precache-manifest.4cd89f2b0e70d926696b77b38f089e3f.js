9119cb8a6fb72aefbd679f2d91989e8d331a7373
Irina Lysenko <irina.lysenko21@gmail.com>
2020-01-12 16:28:47 +0200

    Updates

---
+++ precache-manifest.4cd89f2b0e70d926696b77b38f089e3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "244b0ead37bf01e44c33cd48e6084da7",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "fd6b50831b5aa3bc34b0",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f0960bb599576874d8ae",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.57b9da29.chunk.css"
+  },
+  {
+    "revision": "fd6b50831b5aa3bc34b0",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.6d130e92.chunk.js"
+  },
+  {
+    "revision": "f0960bb599576874d8ae",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.fe1b7906.chunk.js"
+  },
+  {
+    "revision": "191fa215192447a8301c",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.3b37d30b.js"
+  }
+]);
